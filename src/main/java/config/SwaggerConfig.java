11189ad5db0ee38d221221ab3d8470b3e79b85cf
Duban-sg <universidad.duban@gmail.com>
2022-11-07 18:31:39 -0500

    Intento de configuracion de swagger

---
+++ src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+    
+    private ApiInfo getApiInfo() {
+		return new ApiInfo(
+				"Order Service API",
+				"Order Service API Description",
+				"1.0",
+				"http://codmind.com/terms",
+				new Contact("Codmind", "https://codmind.com", "apis@codmind.com"),
+				"LICENSE",
+				"LICENSE URL",
+				Collections.emptyList()
+				);
+	}
+}
